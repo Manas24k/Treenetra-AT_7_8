8ac802daf25990bb96a6503402da1955b20a5b4c
manas rout <manasrout057@gmail.com>
2024-01-21 10:33:16 +0530

    python core notes

---
+++ Operator/Logicalopt.py
@@ -0,0 +1,5 @@
+a=10
+b=20
+c=30
+if a>b or a<c:
+    print('print the value')
---
+++ moduledemo.py
@@ -0,0 +1,7 @@
+import calendar
+import os
+import math
+print(math.sqrt(2))
+print(os.getcwd())
+result=calendar.month(2023,12)
+print(result)
